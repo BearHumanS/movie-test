c788229d7f25ca21050973ea05be3c141595c72c
BearHumanS <bye0651@naver.com>
2023-05-10 21:54:42 +0900

    page 호출 issue fix

--- src/main.js
+++ src/main.js
@@ -1,76 +1,98 @@
-const header = document.querySelector('header')
-const searchContainer = document.querySelector('.search-container')
-const close = document.querySelector('.close')
-const shadow = document.querySelector('.shadow')
-const searchInput = document.querySelector('.search-input')
+const header = document.querySelector('header');
+const searchContainer = document.querySelector('.search-container');
+const close = document.querySelector('.close');
+const shadow = document.querySelector('.shadow');
+const searchInput = document.querySelector('.search-input');
 
-searchContainer.addEventListener('click', addSearching)
-close.addEventListener('click', removeSearching)
-shadow.addEventListener('click', removeSearching)
+searchContainer.addEventListener('click', addSearching);
+close.addEventListener('click', removeSearching);
+shadow.addEventListener('click', removeSearching);
 
 function addSearching() {
-  header.classList.add('searching')
+  header.classList.add('searching');
   setTimeout(() => {
-    searchInput.focus()
-  }, 500)
+    searchInput.focus();
+  }, 500);
 }
+
 function removeSearching() {
-  header.classList.remove('searching')
+  header.classList.remove('searching');
 }
 
-const select = document.querySelector('#years')
-const now = new Date()
-const thisYear = now.getFullYear()
+const select = document.querySelector('#years');
+const now = new Date();
+const thisYear = now.getFullYear();
 for (let year = thisYear; year >= 1950; year--) {
-  const option = document.createElement('option')
-  option.value = year
-  option.textContent = year
-  select.appendChild(option)
+  const option = document.createElement('option');
+  option.value = year;
+  option.textContent = year;
+  select.appendChild(option);
 }
 
-const apiKey = '7035c60c'
-const movieList = document.querySelector('#movie-list')
+const apiKey = '7035c60c';
+const movieList = document.querySelector('#movie-list');
 
 async function fetchMovies(searchInputVal, page, year) {
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${apiKey}&s=${searchInputVal}&page=${page}&y=${year}`
-  )
-  const data = await response.json()
-  return data.Search
+  const numPages = parseInt(document.querySelector('#num-pages').value);
+  if (![1, 2, 3].includes(numPages)) {
+    return [];
   }
+
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=${apiKey}&s=${encodeURIComponent(
+      searchInputVal
+    )}&page=${page}&y=${year}`
+  );
+  const data = await response.json();
+  return data.Search
+}
 
 searchInput.addEventListener('keydown', async e => {
   if (e.key === 'Enter') {
-    removeSearching()
-    const searchInputVal = searchInput.value
+    removeSearching();
+    const searchInputVal = searchInput.value;
     if (searchInputVal.trim() === '') {
-      alert('검색어를 입력해주세요.')
-      return
+      alert('검색어를 입력해주세요.');
+      return;
     }
     try {
-      const year = select.value
-      const page = parseInt(document.querySelector('#num-pages').value)
-      const data = await fetchMovies(searchInputVal, page, year)
-      const movieItems = data.map(({ Title, Year, imdbID, Poster }) => {
-        return `
-          <div class="swiper-slide">
-            <h2>${Title} (${Year})</h2>
-            <p><img class="poster" src=${Poster} alt="${Title}" onerror="this.onerror=null; this.src='/No-image-available.jpg';" onclick="getDetails('${imdbID}')">
-            </p>
-            <div class="movie-data" style="display: none;"></div>
-          </div>
-        `
-      })
-      movieList.innerHTML = movieItems.join('')
+      const year = select.value;
+      const numPages = parseInt(document.querySelector('#num-pages').value);
+      const movies = [];
+      for (let i = 1; i <= numPages; i++) {
+        const data = await fetchMovies(searchInputVal, i, year);
+        movies.push(...data);
+      }
+
+      if (movies.length === 0) {
+        alert('API 요청에 실패하였거나 페이지 범위를 지정해야 합니다.');
+        return;
+      }
+
+      const movieItems = movies
+        .map(({ Title, Year, imdbID, Poster }) => {
+          return `
+            <div class="swiper-slide">
+              <h2>${Title} (${Year})</h2>
+              <p><img class="poster" src=${Poster} alt="${Title}" onerror="this.onerror=null; this.src='/No-image-available.jpg';" onclick="getDetails('${imdbID}')">
+              </p>
+              <div class="movie-data" style="display: none;"></div>
+            </div>
+          `;
+        })
+        .join('');
+
+      movieList.innerHTML = movieItems;
     } catch (error) {
-      alert('API 요청에 실패했습니다. ', error)
+      alert('API 요청에 실패했습니다. ', error);
     }
   }
-})
+});
+
 async function getDetails(imdbID) {
-  const details = document.querySelector('.movie-data')
+  const details = document.querySelector('.movie-data');
   if (details.style.display === 'block') {
-    details.style.display = 'none'
+    details.style.display = 'none';
   } else {
     details.innerHTML = '<p>Loading...</p>'
     details.style.display = 'block'
